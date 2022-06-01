6d0bd2d4f19f03a5c9029165ab72bcf7558f0299
HDLaZy <2889312795@qq.com>
2022-06-01 11:26:32 +0800

    首次提交

---
+++ src/test/java/com/lk/javacv/JavacvApplicationTests.java
@@ -0,0 +1,13 @@
+package com.lk.javacv;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JavacvApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
